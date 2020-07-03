d6de51600b4f50271d0051d6ce357534a1a00a26
Greg Brosman <greg.brosman@gmail.com>
2020-07-02 20:17:29 -0700

    code to follow back a user.

--- bot_main.py
+++ bot_main.py
@@ -1,6 +1,7 @@
+import time
 import tweepy
 
-# Twitter generated keys.
+# Twitter generated keys for user identification.
 consumer_key = "x4kRrcD8PSM0K3l2IXvBK5RYl"
 consumer_secret = "OJhVTJrrGLCUb43jFD5ICxrwdaDnuLlEYh4HmPKpyrfToELzmR"
 access_token = "351024115-irq5NUArNqxgQB8QmBX9hfrAVfyAXM4ZyEfjl67t"
@@ -10,7 +11,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+user = api.me()
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+
+for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+    if follower.name not in follower:
+        follower.follow(follower.name)
+        break
+
+
+# Prints tweets from homepage of timeline.
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
